932f9619ad7ee9b025c167ee02b6cc8ff544f1b3
Nick-0201 <nurmukhammad93@icloud.com>
2023-06-15 13:02:29 +0600

    newStart

--- js/array01.js
+++ js/array01.js
@@ -182,88 +182,88 @@
 
 //= = = = = = = ----- домашка создать дроп даун на минималках -----= = = = = = =\
 
-const outBtn = document.querySelector(".out-block__btn")
-const block = document.querySelector(".in-block")
-console.log(block);
-outBtn.addEventListener("click", () => {
-  if (block.className == "in-block none") {
-    block.className = "in-block"
-  } else { block.className += " none" }
-});
+// const outBtn = document.querySelector(".out-block__btn")
+// const block = document.querySelector(".in-block")
+// console.log(block);
+// outBtn.addEventListener("click", () => {
+//   if (block.className == "in-block none") {
+//     block.className = "in-block"
+//   } else { block.className += " none" }
+// });
 
-const max = ["qerty", 123, false, null];
-const items = [
-  {
-    name: "bananana",
-    price: 200,
-    category: "food",
-  },
-  {
-    name: "fish",
-    price: 1200,
-    category: "food",
-  },
-  {
-    name: "pen",
-    price: 20,
-    category: "office",
-  },
-  {
-    name: "pencil",
-    price: 20,
-    category: "office",
-  },
-  {
-    name: "cola",
-    price: 80,
-    category: "drinks",
-  },
-  {
-    name: "fanta",
-    price: 80,
-    category: "drinks",
-  },
-];
+// const max = ["qerty", 123, false, null];
+// const items = [
+//   {
+//     name: "bananana",
+//     price: 200,
+//     category: "food",
+//   },
+//   {
+//     name: "fish",
+//     price: 1200,
+//     category: "food",
+//   },
+//   {
+//     name: "pen",
+//     price: 20,
+//     category: "office",
+//   },
+//   {
+//     name: "pencil",
+//     price: 20,
+//     category: "office",
+//   },
+//   {
+//     name: "cola",
+//     price: 80,
+//     category: "drinks",
+//   },
+//   {
+//     name: "fanta",
+//     price: 80,
+//     category: "drinks",
+//   },
+// ];
 
-const list = document.querySelector(".in-block__list");
+// const list = document.querySelector(".in-block__list");
 
-for (let i = 0; i < items.length; i++) {
-  list.innerHTML += "<li>" + items[i].name + " " + items[i].category + " " + items[i].price + "</li>"
-}
-
-
-const inBtn = document.querySelector(".in-block__btn")
-const inName = document.querySelector(".in-name")
-const inPrice = document.querySelector(".in-price")
-const inCategory = document.querySelector(".in-category")
-
-inBtn.addEventListener("click", () => {
-
-  list.innerHTML = ""
-
-  items.push({
-    name: inName.value,
-    price: inPrice.value,
-    category: inCategory.value,
-  });
-
-  //= = = = = = = ----- Пытаюсь оставить placeholder после заполнения поле. -----= = = = = = =\
-  //= = = = = = = --------------------А он сволочь исчезает.----------------------= = = = = = =\
-
-  inName.value = "";
-  inPrice.value = "";
-  inCategory.value = "";
-  //= = = = = = = ----- смотрел как обратиться к placeholder -----= = = = = = =\
-  console.log([inCategory]);
-  // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =\
-  console.log(items);
-
-  for (let i = 0; i < items.length; i++) {
-    list.innerHTML += "<li>" + items[i].name + " " + items[i].category + " " + items[i].price + "</li>"
-  }
+// for (let i = 0; i < items.length; i++) {
+//   list.innerHTML += "<li>" + items[i].name + " " + items[i].category + " " + items[i].price + "</li>"
+// }
 
 
-})
+// const inBtn = document.querySelector(".in-block__btn")
+// const inName = document.querySelector(".in-name")
+// const inPrice = document.querySelector(".in-price")
+// const inCategory = document.querySelector(".in-category")
+
+// inBtn.addEventListener("click", () => {
+
+//   list.innerHTML = ""
+
+//   items.push({
+//     name: inName.value,
+//     price: inPrice.value,
+//     category: inCategory.value,
+//   });
+
+//   //= = = = = = = ----- Пытаюсь оставить placeholder после заполнения поле. -----= = = = = = =\
+//   //= = = = = = = --------------------А он сволочь исчезает.----------------------= = = = = = =\
+
+//   inName.value = "";
+//   inPrice.value = "";
+//   inCategory.value = "";
+//   //= = = = = = = ----- смотрел как обратиться к placeholder -----= = = = = = =\
+//   console.log([inCategory]);
+//   // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =\
+//   console.log(items);
+
+//   for (let i = 0; i < items.length; i++) {
+//     list.innerHTML += "<li>" + items[i].name + " " + items[i].category + " " + items[i].price + "</li>"
+//   }
+
+
+// })
 
 
 
