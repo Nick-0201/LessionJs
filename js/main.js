64120e006cccca7c2e27b055f2ec876f47d890c8
Nick-0201 <nurmukhammad93@icloud.com>
2023-06-19 23:30:21 +0600

    i can`t do it

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 import { burgerExpo } from './burger.js'
-
 burgerExpo();
+
+// import { arrayMethods } from './arrayMethods.js';
+// arrayMethods();
+
+import { projectExpo } from './projectArray.js';
+projectExpo();
